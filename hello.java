d63327b9341d215d230f20d290c7e86a14276265
Yakanna <167448285+yakanna2000@users.noreply.github.com>
2025-04-09 23:21:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 import java.util.*;
  public class hello{
       public static void main(String[] args){
-           System.out.print("hi");
+           System.out.print("hi hello");
       }
  }
 //  git init
