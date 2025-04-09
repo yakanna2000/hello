a3623a05db6b5e07919bb6daacf7b644e3e0d56e
yakanna <yakannakadem@gmail.com>
2025-04-09 23:18:30 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ public class hello{
+      public static void main(String[] args){
+           System.out.print("hi");
+      }
+ }
+//  git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/yakanna2000/hello.git
+// git push -u origin main
